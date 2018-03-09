3957b11bb1b963ac3890933697f8f27e1dee745d
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-09 21:03:32 +0000

    Added list id to card model

---
+++ db/migrate/20180309210047_add_list_id_to_cards.rb
@@ -0,0 +1,5 @@
+class AddListIdToCards < ActiveRecord::Migration[5.1]
+  def change
+    add_column :cards, :list_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309203123) do
+ActiveRecord::Schema.define(version: 20180309210047) do
 
   create_table "cards", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180309203123) do
     t.boolean "overdue", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "list_id"
   end
 
   create_table "lists", force: :cascade do |t|
