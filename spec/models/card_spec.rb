233aba14871b2b1b257ecf9b6e26b6504324bd27
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-09 20:44:56 +0000

    Test for default values of card

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Card, type: :model do
   it {  should validate_presence_of :title }
   it {  should validate_presence_of :description }
+
+  context 'default values' do
+    subject(:card){ described_class.new }
+    it 'should have a default false value for the attribute due_date_soon' do
+      expect(card.due_date_soon).to eq false
+    end
+
+    it 'should have a default false value for the attribute overdue' do
+      expect(card.overdue).to eq false
+    end
+  end
 end
