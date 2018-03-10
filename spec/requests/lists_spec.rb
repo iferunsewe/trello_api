0b2b00d6d09d16997a69a303bad0abdcc204969e
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-10 21:29:23 +0000

    request specs and first get request test for lists

---
+++ spec/requests/lists_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Lists API', type: :request do
+
+  describe 'GET /lists (all the lists)' do
+    let(:number_of_lists) { 5 }
+    let!(:lists) {  create_list(:list, number_of_lists) }
+    let(:parsed_json){ JSON.parse(response.body) }
+
+    before { get '/lists' }
+    it 'returns all the lists' do
+      expect(parsed_json.size).to eq(number_of_lists)
+    end
+  end
+
+end
