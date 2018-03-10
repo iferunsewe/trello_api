88cd9dd545f7e468e7314730816b831ea7dc130b
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-10 20:51:51 +0000

    more details for card factory

---
+++ spec/factories/cards.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :card do
+    title 'Bar'
+    description 'Duis ornare, neque vel tincidunt suscipit, risus velit venenatis augue, nec consectetur enim ipsum eget neque. Donec pharetra in leo ac placerat'
+    due_date Date.today + 7
+  end
+end
