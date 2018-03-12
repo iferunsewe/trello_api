4e1ed43dc5bc0c39f8ff5a31fecd6c91b2b49c5c
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-12 15:03:29 +0000

    Testing update and delete action

--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -79,9 +79,9 @@ RSpec.describe 'Cards API', type: :request do
     before do
       put "/lists/#{list_id}/cards/#{card_id}", params: params
     end
+    let(:params){ { title: 'New title' } }
 
     context 'when the card exists' do
-      let(:params){ { title: 'New title' } }
       it 'returns a 200 status code' do
         expect_status_code?(200)
       end
@@ -89,6 +89,18 @@ RSpec.describe 'Cards API', type: :request do
       it 'returns the card with the changed attribute' do
         expect(parsed_json['id']).to eq(card_id)
         expect(parsed_json['title']).to eq(params[:title])
+      end
+    end
+
+    context 'when the card does not exist' do
+      let(:card_id) { number_of_cards + 1 }
+
+      it 'returns a 404 status code' do
+        expect_status_code?(404)
+      end
+
+      it 'returns a message to say the card could not be found' do
+        expect(parsed_json['error']).to include "Couldn't find Card with 'id'=#{card_id}"
       end
     end
   end
@@ -100,6 +112,16 @@ RSpec.describe 'Cards API', type: :request do
       expect(Card.find(card_id).list_id).to eq list_id
       put "/cards/#{card_id}/change_list/#{new_list.id}"
       expect(parsed_json['list_id']).to eq(new_list.id)
+    end
+  end
+
+  describe 'delete a card in a list' do
+    before do
+      delete "/lists/#{list_id}/cards/#{card_id}"
+    end
+
+    it 'returns a 204 status code' do
+      expect_status_code?(204)
     end
   end
 
